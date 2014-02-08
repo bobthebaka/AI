068c26e55056c0956a10452765493c02273c2b28
lcarloscastillo <lcarlos.castillo@outlook.com>
2014-02-07 21:21:06 -0600

    Fixed SmothMovement script
    it now applies the translation to the x,y axis of the space in world,
    not on itself

--- Assets/scripts/SmoothMovement.cs
+++ Assets/scripts/SmoothMovement.cs
@@ -12,14 +12,14 @@ public class SmoothMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Speed = 0;
-		direction= new Vector2(-1f,0f);
+		direction= new Vector2(1f,1f);
 		Xposition = box.transform.position.x;
 			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		box.transform.Translate(direction * Time.deltaTime * Speed);
+		box.transform.Translate(direction * Time.deltaTime * Speed,Space.World);
 		Xposition = box.transform.position.x;
 
 		if (Xposition >= 5)
